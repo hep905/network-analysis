4b6551b9561dba25f3b8737d0b6b0c634d19f3c6
Peng He <105131054+hep905@users.noreply.github.com>
2022-07-14 16:06:06 -0400

    Create Peng_example in Lab 1 presentation.R

---
+++ Peng_example in Lab 1 presentation.R
@@ -0,0 +1,45 @@
+install.packages("readxl")
+install.packages("tidygraph")
+install.packages("ggraph")
+
+library(readxl)
+library(tidygraph)
+library(ggraph)
+library(tidyverse)
+
+student_nodes <- read_excel("lab-1/data/student-attributes.xlsx")
+student_edges <- read_excel("lab-1/data/student-edgelist.xlsx")
+
+View(student_edges)
+View(student_nodes)
+
+student_network <- tbl_graph(edges = student_edges,
+                             nodes = student_nodes, 
+                             directed = TRUE)
+student_network
+
+plot(student_network)
+
+autograph(student_network)
+
+autograph(student_network,
+          node_label = id,
+          node_colour = gender)
+
+ggraph(student_network) + 
+  geom_node_point()
+
+ggraph(student_network) + 
+  geom_node_point() + 
+  geom_edge_link()
+
+ggraph(student_network, layout = "stress") + 
+  geom_edge_link(arrow = arrow(length = unit(1, 'mm')), 
+                 end_cap = circle(3, 'mm'),
+                 start_cap = circle(3, 'mm'),
+                 alpha = .1) +
+  geom_node_point(aes(size = local_size(),
+                      color = gender)) +
+  geom_node_text(aes(label = id),
+                 repel=TRUE) +
+  theme_graph()
